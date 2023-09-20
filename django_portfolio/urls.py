de740f5ad811466f1ab493de35bfb21abd96f40f
Bryanbaque <e1206865683@live.uleam.edu.ec>
2023-09-19 20:06:23 -0500

    Prueba_Img

--- django_portfolio/urls.py
+++ django_portfolio/urls.py
@@ -25,4 +25,7 @@ urlpatterns = [
     path('',views.home, name='home'),
     path('blog/', include('blog.urls'))
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
